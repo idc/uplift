45f6febf389ff9adf320806db86b969002fcc4ac
idc <idc@users.noreply.github.com>
2017-11-25 05:49:35 -0600

    Align helpers.

--- uplift/src/helpers.hpp
+++ uplift/src/helpers.hpp
@@ -8,6 +8,20 @@
 
 namespace uplift
 {
+  constexpr size_t align_size_const(size_t size, size_t align)
+  {
+    const size_t padding = align - 1;
+    const size_t mask = ~padding;
+    return (size + padding) & mask;
+  }
+
+  size_t __forceinline align_size(size_t size, const size_t align)
+  {
+    const size_t padding = align - 1;
+    const size_t mask = ~padding;
+    return (size + padding) & mask;
+  }
+
   xe::memory::PageAccess get_page_access(llvm::ELF::Elf64_Word flags);
   
   bool get_text_region(uint8_t* buffer, size_t buffer_size, uint8_t*& text, size_t& text_size);
